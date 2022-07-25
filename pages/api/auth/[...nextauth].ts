9a6726076144056f6e2c176238513fe2d1f0bba0
locoer <locoer@gmail.com>
2022-07-25 16:58:09 -0500

    Added TypeORM configuration (with PostgreSQL) to NextAuth. Using custom Entities

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,12 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { TypeORMLegacyAdapter } from "@next-auth/typeorm-legacy-adapter"
+import {ConnectionObject} from '../../../utils/db/data-source'
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
+  adapter: TypeORMLegacyAdapter( ConnectionObject ),
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     GoogleProvider({
