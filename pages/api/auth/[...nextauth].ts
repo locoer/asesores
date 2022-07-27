6da68edafc98e18773a3d8b54ab3f029f43e5857
locoer <locoer@gmail.com>
2022-07-27 17:24:48 -0500

    Added next-auth email provider. Tested with Mailtrap. Updated .env.example

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email";
 import { TypeORMLegacyAdapter } from "@next-auth/typeorm-legacy-adapter"
 import {ConnectionObject} from '../../../utils/db/data-source'
 
@@ -12,6 +13,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
     })
   ],
   theme: {
